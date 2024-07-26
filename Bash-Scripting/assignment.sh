efb8aab1f0b342cc19a4879fcc049fee8eca5bda
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-26 09:59:08 +0300

    Bash scripting

---
+++ Bash-Scripting/assignment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create a directory called Lab1_Dir
+mkdir Lab1_Dir
+
+# Change the current directory to Lab1_Dir
+cd Lab1_Dir
+
+# Creating a file named testfile.txt
+touch testfile.txt
+
+# Writing into the file
+echo Hello World! > testfile.txt
+
+# Display the contents of testfile.txt
+cat testfile.txt
+
+# List the files in Lab1_Dir
+ls 
+
